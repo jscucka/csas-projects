d14d5d204b22451e8a10e2505ebf73f5408490b3
jscucka <scucka.honza@gmail.com>
2023-04-07 22:08:04 +0200

    Adding new file for validation

---
+++ csas-projects/src/utils/formSchema.ts
@@ -0,0 +1,25 @@
+import { z, ZodType } from "zod";
+
+export const formSchema: ZodType = z
+  .object({
+    firstName: z
+      .string()
+      .min(2, { message: "Jméno musí mít aspoň 2 znaky" })
+      .max(20, { message: "Jméno musí být menší než 20 znaků." }),
+    lastName: z
+      .string()
+      .min(2, { message: "Příjmení musí mít aspoň 2 znaky" })
+      .max(20, { message: "Příjmení musí být menší než 20 znaků." }),
+    email: z.string().email({ message: "Nevalidní email." }).optional(),
+    age: z.number().min(18, { message: "Musí ti bý 18." }),
+    password: z
+      .string()
+      .min(4, { message: "Heslo musí být delší než 3 znaky." }),
+    confirmPassword: z
+      .string()
+      .min(4, { message: "Heslo musí být delší než 3 znaky." }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Hesla se neshodují",
+    path: ["confirmPassword"],
+  });
